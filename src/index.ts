c4d55e983773f1b98493e8b1231b29ba52731d7c
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2024-10-04 17:32:54 +0200

    fix: ability to stringify bigints

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 //Exports all handler functions
 export * from "./mappings/mappingHandlers";
+
+// Workaround to fix BigInt serialization issue
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt#use_within_json
+(BigInt.prototype as any).toJSON = function () {
+    return this.toString();
+};
